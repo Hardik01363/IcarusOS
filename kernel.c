56157946c48f36878b4c90ad1ae93268b776e067
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-08-04 23:23:05 +0530

    OpenSBI API and registers initialised

--- kernel.c
+++ kernel.c
@@ -1,27 +1,14 @@
-typedef unsigned char uint8_t;
-typedef unsigned int uint32_t;
-typedef uint32_t size_t;
+#include "kernel.h"
 
 extern char __bss[], __bss_end[], __stack_top[]; //__bss alone would mean value of 0th byte of .bss section. To get start address of .bss section, we add the [] at the end
 
-void* memset(void* buff, char c, size_t n) {
-    uint8_t* p = (uint8_t*)buff;
-    while(n--) {*p++ = c;}
-    return buff;
-}
-
-void kernel_main(void) {
-    memset(__bss, 0, (size_t)__bss_end - (size_t)__bss); //.bss section initialised to 0. Some bootloders may recognise and 0-clear the .bss section, but, we do it manually too just in case the bootloader doesnt.
-    for(;;);
-}
-
-__attribute__((section(".text.boot")))
-__attribute__((naked))
-void boot(void) {
-    __asm__ __volatile__(
-        "mv sp, %[stack_top]\n" //setting the stack pointer
-        "j kernel_main\n"
-        : //jump conditional protocols to follow
-        : [stack_top] "r" (__stack_top) //passing stack_top address as %[stack_top]
-    );
+struct sbi_ret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid) {
+    register long a0 __asm__("a0") = arg0;
+    register long a1 __asm__("a0") = arg1;
+    register long a2 __asm__("a0") = arg2;
+    register long a3 __asm__("a0") = arg3;
+    register long a4 __asm__("a0") = arg4;
+    register long a5 __asm__("a0") = arg5;
+    register long a6 __asm__("a0") = fid;
+    register long a7 __asm__("a0") = eid;
 }
---
+++ kernel.h
@@ -0,0 +1,3 @@
+#pragma once
+
+struct sbi_ret {long error, value;}

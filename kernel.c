75d2df7776b2071f1c2eef92a22abb025c413763
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-08-08 16:07:14 +0530

    sbi_call done

--- kernel.c
+++ kernel.c
@@ -4,11 +4,24 @@ extern char __bss[], __bss_end[], __stack_top[]; //__bss alone would mean value 
 
 struct sbi_ret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid) {
     register long a0 __asm__("a0") = arg0;
-    register long a1 __asm__("a0") = arg1;
-    register long a2 __asm__("a0") = arg2;
-    register long a3 __asm__("a0") = arg3;
-    register long a4 __asm__("a0") = arg4;
-    register long a5 __asm__("a0") = arg5;
-    register long a6 __asm__("a0") = fid;
-    register long a7 __asm__("a0") = eid;
+    register long a1 __asm__("a1") = arg1;
+    register long a2 __asm__("a2") = arg2;
+    register long a3 __asm__("a3") = arg3;
+    register long a4 __asm__("a4") = arg4;
+    register long a5 __asm__("a5") = arg5;
+    register long a6 __asm__("a6") = fid;
+    register long a7 __asm__("a7") = eid;
+
+    __asm__ __volatile__(
+        "ecall"
+        : "=r"(a0), "=r"(a1)
+        : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7)
+        : "memory"
+    );
+
+    return (struct sbi_ret){.error = a0, .value = a1};
+}
+
+void put_char(char c) {
+    sbi_call(c, 0,0,0,0,0,0, 1);
 }
--- kernel.h
+++ kernel.h
@@ -1,3 +1,3 @@
 #pragma once
 
-struct sbi_ret {long error, value;}
+struct sbi_ret {long error, value;};

2e464012fca5ad537532b88811e33a4119c53ab1
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-08-09 16:28:50 +0530

    now prints hello world

--- kernel.c
+++ kernel.c
@@ -1,6 +1,16 @@
 #include "kernel.h"
 
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+typedef uint32_t size_t;
+
 extern char __bss[], __bss_end[], __stack_top[]; //__bss alone would mean value of 0th byte of .bss section. To get start address of .bss section, we add the [] at the end
+
+void* memset(void* buff, char c, size_t n) {
+    uint8_t* p = (uint8_t*)buff;
+    while(n--) {*p++ = c;}
+    return buff;
+}
 
 //sbi_call implemented accordin to OpenSBI calling convention. SBI can only change values of a0, a1 registers. a2-a7 reg values remain same after the call.
 struct sbi_ret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid) {
@@ -28,7 +38,19 @@ void put_char(char c) {
 }
 
 void kernel_main(void) {
+    memset(__bss, 0, (size_t)__bss_end - (size_t)__bss); //.bss section initialised to 0. Some bootloders may recognise and 0-clear the .bss section, but, we do it manually too just in case the bootloader doesnt.
     const char* s = "\n\nKonnichiwa Sekai!\n";
     for(int i = 0; s[i] != '\0'; i++) {put_char(s[i]);}
     for(;;) {__asm__ __volatile("wfi");}
+}
+
+__attribute__((section(".text.boot")))
+__attribute__((naked))
+void boot(void) {
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n" //setting the stack pointer
+        "j kernel_main\n"
+        : //jump conditional protocols to follow
+        : [stack_top] "r" (__stack_top) //passing stack_top address as %[stack_top]
+    );
 }

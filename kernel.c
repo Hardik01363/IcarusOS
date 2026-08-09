e49b48996abc0c14b511f1367a943eddca787a4c
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-08-09 16:21:19 +0530

    debugging needed

--- kernel.c
+++ kernel.c
@@ -2,6 +2,7 @@
 
 extern char __bss[], __bss_end[], __stack_top[]; //__bss alone would mean value of 0th byte of .bss section. To get start address of .bss section, we add the [] at the end
 
+//sbi_call implemented accordin to OpenSBI calling convention. SBI can only change values of a0, a1 registers. a2-a7 reg values remain same after the call.
 struct sbi_ret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid) {
     register long a0 __asm__("a0") = arg0;
     register long a1 __asm__("a1") = arg1;
@@ -9,8 +10,8 @@ struct sbi_ret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, l
     register long a3 __asm__("a3") = arg3;
     register long a4 __asm__("a4") = arg4;
     register long a5 __asm__("a5") = arg5;
-    register long a6 __asm__("a6") = fid;
-    register long a7 __asm__("a7") = eid;
+    register long a6 __asm__("a6") = fid; //SBI function ID
+    register long a7 __asm__("a7") = eid; //SBI extension ID
 
     __asm__ __volatile__(
         "ecall"
@@ -24,4 +25,10 @@ struct sbi_ret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, l
 
 void put_char(char c) {
     sbi_call(c, 0,0,0,0,0,0, 1);
+}
+
+void kernel_main(void) {
+    const char* s = "\n\nKonnichiwa Sekai!\n";
+    for(int i = 0; s[i] != '\0'; i++) {put_char(s[i]);}
+    for(;;) {__asm__ __volatile("wfi");}
 }

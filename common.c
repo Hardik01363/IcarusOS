489c8ca1aaaa4595d9ca54f407b6affe1228e528
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-08-11 21:22:37 +0530

    printf: function skeleton declared

---
+++ common.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+void putchar(char c);
+
+void printf(const char* fmtd_str, ...) {
+    
+}
---
+++ common.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//__builtin_ provided by clang compiler
+#define va_list __builtin_va_list
+#define va_start __builtin_va_start
+#define va_end __builtin_va_end
+#define va_arg __builtin_va_arg
+
+void printf(const char* frmtd_str, ...);

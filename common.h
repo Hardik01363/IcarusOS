6f1f557b512aceed3e32330cf0f22850a59e5818
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-09-24 16:05:05 +0530

    taking a break

--- common.h
+++ common.h
@@ -1,5 +1,14 @@
 #pragma once
 
+typedef int bool
+typedef unsigned char uint8_t
+typedef unsigned short uint16_t
+typedef unsigned int uint32_t
+typedef unsigned long long uint64_t
+typedef uint32_t size_t
+typedef uint32_t paddr_t
+typedef uint32_t vaddr_t
+
 //__builtin_'s provided by clang compiler, defined in C standard library's <stdarg.h>.
 //we can use compiler builtins without relying on standard library :)
 #define va_list __builtin_va_list
